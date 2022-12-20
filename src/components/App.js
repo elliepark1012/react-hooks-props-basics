6108fee952a2c5d7c753f5491cadb41b42de3f8a
Ellie Park <elliepark1012@gmail.com>
2022-12-20 13:09:54 -0500

    props studying

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import BlogPost from "./BlogPost";
 
 function App() {
-  return <BlogPost />;
+  return <BlogPost />;//only return one component on App.js
 }
 
 export default App;
